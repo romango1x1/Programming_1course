38222548c482ed32b88afc3a87f2b812be80cc17
Roman Suhoniak <144212889+romango1x1@users.noreply.github.com>
2023-09-15 20:50:00 +0300

    Delete Homework 1 directory

--- Homework 1/suhoniak_1_14.py
+++
@@ -1,8 +0,0 @@
-x, y = [float(nvm) for nvm in input().split()]
-a = (2 * x * y) / ((x ** 2 + y ** 2) ** 0.5)
-b = ((x + y - 1) ** 2) / (x * y)
-c = a - b
-
-print(round(c, 3))
-
-# https://www.eolymp.com/uk/submissions/14225622
