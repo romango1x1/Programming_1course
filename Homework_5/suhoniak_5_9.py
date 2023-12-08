c7569ec5c2b1c79163f5fa9c0cd088d5134e4450
Roman Suhoniak <144212889+romango1x1@users.noreply.github.com>
2023-12-08 03:06:49 +0200

    Fixed code
    Read the problem description closer

--- Homework_5/suhoniak_5_9.py
+++ Homework_5/suhoniak_5_9.py
@@ -1,8 +1,8 @@
 a, b, c, d = map(int, input().split())
 prods = []
 
-for i in range(a, b + 1):
-    for j in range(c, d + 1):
+for i in range(min(a, b), (max(a, b) + 1)):
+    for j in range(min(c, d), (max(c, d)) + 1):
         res = i * j
         if res not in prods:
             prods.append(res)
